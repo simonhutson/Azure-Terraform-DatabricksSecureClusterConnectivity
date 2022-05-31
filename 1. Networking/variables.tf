ec89382d871af1620ea7e68e975e09952d9a242d
Simon Hutson <simonhutson@users.noreply.github.com>
2022-05-31 17:35:09 +0100

    Update variables.tf

--- 1. Networking/variables.tf
+++ 1. Networking/variables.tf
@@ -1,29 +1,31 @@
 variable "network_tags" {
   type = map(any)
   default = {
-    "Application ID" : "AP123456",
-    "Application Name" : "Elimin8Cancer PoC",
-    "Application Owner" : "James Wake",
-    "Cost Center" : "123456",
-    "Environment" : "POC",
-    "Organization" : "Oncology R&D"
+    "Application ID" : "",
+    "Application Name" : "",
+    "Application Owner" : "",
+    "Cost Center" : "",
+    "Creation Date" : "",
+    "Environment" : "",
+    "IDMD No" : "",
+    "Organization" : ""
   }
 }
 variable "nsg_subscription_id" {
   type    = string
-  default = "25d7b58f-a2ed-4c46-a3f7-1748a5e0934b"
+  default = "72ed9c19-6e49-4acd-98f2-0859cf924b5b"
 }
 variable "nsg_resource_group_name" {
   type    = string
-  default = "SRH-CORE"
+  default = "az-preview-eu-networking-rsg"
 }
 variable "nsg_name" {
   type    = string
-  default = "SRH-NSG-DATABRICKS"
+  default = "az-preview-eu-databricks-nsg"
 }
 variable "nsg_location" {
   type    = string
-  default = "westeurope"
+  default = "uksouth"
 }
 variable "route_table_subscription_id" {
   type    = string
@@ -35,57 +37,57 @@ variable "route_table_resource_group_name" {
 }
 variable "route_table_name" {
   type    = string
-  default = "SRH-RT-DATABRICKS"
+  default = "az-preview-eu-databrickssubnet-rt"
 }
 variable "route_table_location" {
   type    = string
-  default = "westeurope"
+  default = "uksouth"
 }
 variable "firewall_subscription_id" {
   type    = string
-  default = "25d7b58f-a2ed-4c46-a3f7-1748a5e0934b"
+  default = "e63a6060-c456-481c-bfd1-92b73a1d389e"
 }
 variable "firewall_resource_group_name" {
   type    = string
-  default = "SRH-CORE"
+  default = "az-preview-eu-networking-rsg"
 }
 variable "firewall_name" {
   type    = string
-  default = "SRH-FIREWALL-01"
+  default = "az-hub-eu-firewall"
 }
 variable "vnet_subscription_id" {
   type    = string
-  default = "25d7b58f-a2ed-4c46-a3f7-1748a5e0934b"
+  default = "72ed9c19-6e49-4acd-98f2-0859cf924b5b"
 }
 variable "vnet_resource_group_name" {
   type    = string
-  default = "SRH-CORE"
+  default = "az-preview-eu-networking-rsg"
 }
 variable "vnet_name" {
   type    = string
-  default = "SRH-VNET-WESTEUROPE"
+  default = "az-preview-eu-vnet"
 }
 variable "databricks_private_subnet_name" {
   type    = string
-  default = "DATABRICKS-01-PRIVATE"
+  default = "az-preview-eu-databricks-private-subnet"
 }
 variable "databricks_public_subnet_name" {
   type    = string
-  default = "DATABRICKS-01-PUBLIC"
+  default = "az-preview-eu-databricks-public-subnet"
 }
 variable "databricks_subnet_name" {
   type    = string
-  default = "DATABRICKS-01"
+  default = "az-preview-eu-databricks-subnet"
 }
 variable "databricks_private_subnet_prefix" {
   type    = string
-  default = "10.2.2.0/24"
+  default = "10.211.246.0/26"
 }
 variable "databricks_public_subnet_prefix" {
   type    = string
-  default = "10.2.3.0/24"
+  default = "10.211.246.64/26"
 }
 variable "databricks_subnet_prefix" {
   type    = string
-  default = "10.2.4.0/24"
+  default = "10.211.246.128/26"
 }
