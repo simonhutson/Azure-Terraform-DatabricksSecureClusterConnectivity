269fa79b50f0adaf24a7a945a6680d95cce11814
Simon Hutson <simonhutson@users.noreply.github.com>
2022-05-31 18:17:30 +0100

    Update variables.tf

--- 6. DataFactory/variables.tf
+++ 6. DataFactory/variables.tf
@@ -1,59 +1,61 @@
 variable "datafactory_tags" {
   type = map(any)
   default = {
-    "Application ID" : "AP123456",
-    "Application Name" : "Elimin8Cancer PoC",
-    "Application Owner" : "James Wake",
-    "Cost Center" : "123456",
-    "Environment" : "POC",
-    "Organization" : "Oncology R&D"
+    "Application ID" : "",
+    "Application Name" : "",
+    "Application Owner" : "",
+    "Cost Center" : "",
+    "Creation Date" : "",
+    "Environment" : "",
+    "IDMD No" : "",
+    "Organization" : ""
   }
 }
 variable "datafactory_subscription_id" {
   type    = string
-  default = "25d7b58f-a2ed-4c46-a3f7-1748a5e0934b"
+  default = "72ed9c19-6e49-4acd-98f2-0859cf924b5b"
 }
 variable "datafactory_resource_group_name" {
   type    = string
-  default = "SRH-RG-DATABRICKS-01"
+  default = "az-preview-eu-oncology-rsg"
 }
 variable "datafactory_location" {
   type    = string
-  default = "westeurope"
+  default = "uksouth"
 }
 variable "datafactory_name" {
   type    = string
-  default = "SRH-DATAFACTORY-01"
+  default = "azprev-eu-oncologypoc-df-01"
 }
 variable "mssql_server_subscription_id" {
   type    = string
-  default = "25d7b58f-a2ed-4c46-a3f7-1748a5e0934b"
+  default = "72ed9c19-6e49-4acd-98f2-0859cf924b5b"
 }
 variable "mssql_server_resource_group_name" {
   type    = string
-  default = "SRH-RG-DATABRICKS-01"
+  default = "az-preview-eu-oncology-rsg"
 }
 variable "mssql_server_name" {
   type    = string
-  default = "srh-mssql-server-01"
+  default = "az-preview-eu-oncologypoc-sql-server-01"
 }
 variable "storage_account_subscription_id" {
   type    = string
-  default = "25d7b58f-a2ed-4c46-a3f7-1748a5e0934b"
+  default = "2ed9c19-6e49-4acd-98f2-0859cf924b5b"
 }
 variable "storage_account_resource_group_name" {
   type    = string
-  default = "SRH-RG-DATABRICKS-01"
+  default = "az-preview-eu-oncology-rsg"
 }
 variable "storage_account_name" {
   type    = string
-  default = "srhstoragedatabricks02"
+  default = "azpreuondbw02"
 }
 variable "private_endpoint_sql_name" {
   type    = string
-  default = "SRH-PRIVATEENDPOINT-DATABRICKS-01-SQL-SERVER-01"
+  default = "az-preview-eu-oncologypoc-sql-server-01-ep"
 }
 variable "private_endpoint_storage_account_name" {
   type    = string
-  default = "SRH-PRIVATEENDPOINT-DATABRICKS-01-STORAGE-ACCOUNT-01"
+  default = "az-preview-eu-oncologypoc-storage-account-02-ep"
 }
